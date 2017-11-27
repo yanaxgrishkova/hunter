8aeb60adbf6bb514d4de1caa0b43a4cf0f16a5f2
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-11-27 20:59:55 +0300

    Create hunter.cmake

---
+++ cmake/projects/stack/hunter.cmake
@@ -0,0 +1,27 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    stack_05
+    VERSION
+    "1.1"
+    URL
+    "https://github.com/yanaxgrishkova/stack/archive/v1.1.tar.gz"
+    SHA1
+    c08dc366a403d1b28943d846e3c80cecd852d40d
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+hunter_cmake_args(
+    stack
+    CMAKE_ARGS
+    BUILD_EXAMPLES=NO
+    BUILD_TESTS=NO
+)
+hunter_cacheable(stack)
+hunter_download(PACKAGE_NAME stack)
