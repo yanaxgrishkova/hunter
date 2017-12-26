3288ceee13f3e37ce43f430c38f92c980fbf1281
Yana Grishkova <xyanagrishkovax@gmail.com>
2017-12-26 23:30:41 +0300

    Create hunter.cmake

---
+++ cmake/projects/sorting_algorithms_project/hunter.cmake
@@ -0,0 +1,27 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    sorting_algorithms_project
+    VERSION
+    "1.0"
+    URL
+    "https://github.com/yanaxgrishkova/sorting_algorithms_project/archive/v1.0.tar.gz"
+    SHA1
+    29e1a50f960ffb13f81f699b4c378737766789d7
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+hunter_cmake_args(
+    hashtable
+    CMAKE_ARGS
+    BUILD_EXAMPLES=NO
+    BUILD_TESTS=NO
+)
+hunter_cacheable(sorting_algorithms_project)
+hunter_download(PACKAGE_NAME sorting_algorithms_project)
